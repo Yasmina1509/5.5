8410e04532ffc8a9726a5355ccd7a2ec0e8146ae
Yasmina1509 <xikmatullaevayasmina115@gmail.com>
2024-05-13 17:14:40 +0500

    5.5

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let userName = prompt("Пожалуйста, введите ваше имя:");
+
+
+let h1 = document.getElementById("random");
+
+
+h1.textContent = userName;
+
+
+let body = document.querySelector("body");
+
+
+h1.style.backgroundColor = "red";
+body.style.backgroundColor = "black";
+body.style.display = "flex";
+body.style.justifyContent = "center";
+body.style.alignItems = "center";
+body.style.height = "100vh";
+
+
+
+
+    
+
+
+
